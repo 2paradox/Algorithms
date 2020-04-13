c011c9f4c5d7653ef4836ec8b9aef221067ad197
haka913 <ace95913@gmail.com>
2020-04-14 00:48:49 +0900

    20200413 programmers

---
+++ haka_algo/p_다리를지나는트럭.py
@@ -0,0 +1,26 @@
+from collections import deque
+def solution(bridge_length, weight, truck_weights):
+    truck_weights[::-1]
+    que= deque([0]*bridge_length)
+    second =0
+    current_weight =0
+
+    while que:
+        second+=1
+        truck =que.popleft()
+        if truck:
+            current_weight-=truck
+        if truck_weights:
+            if current_weight+truck_weights[-1]<=weight:
+                current_weight += truck_weights[-1]
+                que.append(truck_weights.pop())
+                # que.insert(0,truck_weights.pop())
+            else:
+                que.append(0)
+                # que.insert(0,0)
+
+    return second
+
+print(solution(bridge_length=2, weight=10, truck_weights=[7,4,5,6]))
+# print(solution(bridge_length=100, weight=100, truck_weights=[10]))
+# print(solution(bridge_length=100, weight=100, truck_weights=[10,10,10,10,10,10,10,10,10,10]))
